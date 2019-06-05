36b9c80f7e552fc20bf63f794b5e2d7cf0820e54
jiangbowen <525034005@qq.com>
2019-06-05 09:49:22 +0800

    info:创建springboot项目

---
+++ src/main/java/com/hamusuta/hellohamusuta/HellohamusutaApplication.java
@@ -0,0 +1,13 @@
+package com.hamusuta.hellohamusuta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellohamusutaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellohamusutaApplication.class, args);
+	}
+
+}
